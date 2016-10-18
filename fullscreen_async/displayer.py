33a1a954a118e5a4adf1fcc0b46d999ec84d0d70
Michal Bultrowicz <michalbultrowicz@gmail.com>
2016-10-18 02:02:38 +0200

    Fullscreen console app: working

--- fullscreen_async/displayer.py
+++ fullscreen_async/displayer.py
@@ -1,50 +1,45 @@
-#!/usr/bin/env python
-"""
-(Python >= 3.5)
-This is an example of how to embed a CommandLineInterface inside an application
-that uses the asyncio eventloop. The ``prompt_toolkit`` library will make sure
-that when other coroutines are writing to stdout, they write above the prompt,
-not destroying the input line.
-This example does several things:
-    1. It starts a simple coroutine, printing a counter to stdout every second.
-    2. It starts a simple input/echo cli loop which reads from stdin.
-Very important is the following patch. If you are passing stdin by reference to
-other parts of the code, make sure that this patch is applied as early as
-possible. ::
-    sys.stdout = cli.stdout_proxy()
-"""
+#!/usr/bin/env python3
+import asyncio
+import random
+import sys
 
 from prompt_toolkit.application import Application
+from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.enums import DEFAULT_BUFFER
 from prompt_toolkit.interface import CommandLineInterface
 from prompt_toolkit.key_binding.manager import KeyBindingManager
 from prompt_toolkit.keys import Keys
-from prompt_toolkit.layout.containers import Window
-from prompt_toolkit.layout.controls import TokenListControl
+from prompt_toolkit.layout.containers import VSplit, Window
+from prompt_toolkit.layout.controls import BufferControl, FillControl
+from prompt_toolkit.layout.dimension import LayoutDimension
 from prompt_toolkit.shortcuts import create_asyncio_eventloop
 from prompt_toolkit.token import Token
 
-import asyncio
-import sys
-
-
-async def print_counter():
-    """
-    Coroutine that prints counters.
-    """
-    i = 0
-    while True:
-        print('Counter: %i' % i)
-        i += 1
-        await asyncio.sleep(2)
 
 KILL_APP = 'sraka'
+ASYNC_BUFFER = 'async_buffer'
 
 loop = asyncio.get_event_loop()
 manager = KeyBindingManager()
 registry = manager.registry
 
-layout = Window(content=TokenListControl(
-        get_tokens=lambda cli: [(Token, 'Hello world')]))
+layout = VSplit([
+    Window(content=BufferControl(buffer_name=DEFAULT_BUFFER)),
+
+    Window(width=LayoutDimension.exact(1),
+           content=FillControl('*', token=Token.Line)),
+    Window(width=LayoutDimension.exact(1),
+           content=FillControl('|', token=Token.Line)),
+    Window(width=LayoutDimension.exact(1),
+           content=FillControl('*', token=Token.Line)),
+
+    Window(content=BufferControl(buffer_name=ASYNC_BUFFER)),
+])
+
+buffers={
+    DEFAULT_BUFFER: Buffer(is_multiline=True),
+    ASYNC_BUFFER: Buffer(is_multiline=True),
+}
 
 
 @registry.add_binding(Keys.ControlC, eager=True)
@@ -52,18 +47,24 @@ def key_close(event):
     event.cli.set_return_value(KILL_APP)
 
 
+eventloop = create_asyncio_eventloop()
+application = Application(buffers=buffers,
+                          key_bindings_registry=registry,
+                          layout=layout,
+                          use_alternate_screen=True)
+cli = CommandLineInterface(
+    application=application,
+    eventloop=eventloop)
+
+
+async def do_background_stuff():
+    while True:
+        buffers[ASYNC_BUFFER].insert_text(random.randint(1, 5) * '*' + '\n')
+        cli.invalidate()
+        await asyncio.sleep(1)
+
+
 async def run_application():
-    eventloop = create_asyncio_eventloop()
-
-    application = Application(key_bindings_registry=registry,
-                              layout=layout)
-
-    cli = CommandLineInterface(
-        application=application,
-        eventloop=eventloop)
-
-    sys.stdout = cli.stdout_proxy()
-
     while True:
         try:
             result = await cli.run_async()
@@ -76,7 +77,7 @@ async def run_application():
 def main():
     app_task =  asyncio.ensure_future(run_application())
 
-    background_task = asyncio.gather(print_counter(), return_exceptions=True)
+    background_task = asyncio.gather(do_background_stuff(), return_exceptions=True)
     loop.run_until_complete(app_task)
 
     background_task.cancel()
