03703b4bd0c0f53a4efa3fa95d22927dac006327
Michal Bultrowicz <michalbultrowicz@gmail.com>
2016-12-07 18:07:48 +0100

    Hug app: get/set with Redis, test failing.

--- hug/app/contact_list/app.py
+++ hug/app/contact_list/app.py
@@ -1,7 +1,9 @@
+import os
 import uuid
 
 import hug
 from marshmallow import Schema, fields
+import redis
 
 
 class Contact(Schema):
@@ -10,17 +12,26 @@ class Contact(Schema):
     phone = fields.Dict()
 
 
+redis_client = redis.Redis(port=os.getenv('REDIS_PORT', 6379))
+
+
 @hug.post('/contacts', status=hug.HTTP_201)
 def create_contact(body: hug.types.MarshmallowSchema(Contact()), response):
+    contact = body
     contact_id = str(uuid.uuid4())
-    body['id'] = contact_id
-    response.location = '/contacts/' + contact_id
-    return body
+    contact['id'] = contact_id
 
+    redis_client.hmset(contact_id, contact)
+
+    response.location = '/contacts/' + contact_id
+    return contact
+
+
+@hug.get('/contacts/{id}')
+def get_contact(id: hug.types.text):
+    return redis_client.hgetall(id)
 
 # def get_app():
 #     api = hug.API(__name__)
 #     hug.post('/contacts', api=api)(create_contact)
 #     return api.http.server()
-
-# TODO put redis address
