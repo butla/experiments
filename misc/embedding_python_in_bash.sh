5717d836e4566ae292bbb2a7cee9de08303d835f
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2018-11-05 13:15:32 +0100

    Embedding Python in Bash.

---
+++ misc/embedding_python_in_bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python_code=$(cat <<EOF
+import re, sys;
+input_ = sys.stdin.read();
+output_ = re.sub(r'(?:\d[a-z])', '[tu była cyfra i literka]', input_);
+print(output_);
+EOF
+)
+
+echo $python_code
+echo asdfd123  f1 askdjfh4378fdg67 | python3 -c "$python_code"
