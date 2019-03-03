12b7d6393e52e9158785dd4753d43cedc761f73d
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2019-03-03 21:09:55 +0000

    Django sample: testing the views.

--- django_sample/polls/tests/test_question_model.py
+++ django_sample/polls/tests/test_question_model.py
@@ -21,14 +21,3 @@ def test_was_published_recently_false_for_future_date():
     future_date = timezone.now() + datetime.timedelta(days=30)
     future_question = Question(pub_date=future_date)
     assert not future_question.was_published_recently()
-
-
-# TODO this will be used below
-# TODO enable database access everywhere. Is db_fixture_maker needed?
-def create_question(text: str, days_old: int):
-    creation_date = timezone.now() + datetime.timedelta(days=days_old)
-    return Question.objects.create(text=text, pub_date=creation_date)
-
-
-# TODO
-#class Questionview
---
+++ django_sample/polls/tests/test_question_view.py
@@ -0,0 +1,56 @@
+import datetime
+
+from django.urls import reverse
+from django.utils import timezone
+import pytest
+
+from polls.models import Question
+
+# TODO rename file to questions index test
+
+
+def create_question(text: str, days_old: int = 0):
+    creation_date = timezone.now() - datetime.timedelta(days=days_old)
+    print(creation_date)
+    return Question.objects.create(text=text, pub_date=creation_date)
+
+
+@pytest.mark.django_db
+def test_no_questions_gives_a_message_in_the_index(client):
+    response = client.get(reverse('polls:index'))
+
+    assert response.status_code == 200
+    assert b"No polls are available." in response.content
+
+
+@pytest.mark.django_db
+def test_past_questions_get_displayed(client):
+    question_1 = create_question('Do you even lift?', 3)
+    question_2 = create_question('What is love?', 12)
+
+    response = client.get(reverse('polls:index'))
+
+    assert response.status_code == 200
+    # This actually doesn't check if anything is displayed, it relies on the template
+    # to be correct, which is uncomfortable for me, so I'll add a better assertion next.
+    # This is just so that I remember about that you introspect contexts in Django.
+    assert list(response.context['latest_questions']) == [question_1, question_2]
+
+    # This is something that doesn't depend on the template necessary being correct, but at the
+    # same it it doesn't assert anything about the layout of the page (template's responsibility)
+    # as well.
+    assert question_1.text.encode() in response.content
+    assert question_2.text.encode() in response.content
+
+
+@pytest.mark.django_db
+def test_future_questions_dont_get_displayed(client):
+    visible_question_text = 'see me'
+    invisible_question_text = "you don't know me"
+    create_question(visible_question_text)
+    create_question(invisible_question_text, days_old=-4)
+
+    response = client.get(reverse('polls:index'))
+
+    assert visible_question_text.encode() in response.content
+    assert invisible_question_text.encode() not in response.content
