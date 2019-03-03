6c8ee59f5b1600675e81059770f8a0ba70de1d73
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2019-03-03 21:38:30 +0000

    Django sample: part 5 finished, detail view filtering.

---
+++ django_sample/polls/tests/test_question_detail_view.py
@@ -0,0 +1,28 @@
+from django.urls import reverse
+import pytest
+
+from .utils import create_question
+
+
+@pytest.mark.django_db
+def test_question_details_returned_if_its_from_the_past(client):
+    question_text = "Do you like bread?"
+    past_question = create_question(question_text)
+
+    response = client.get(
+        reverse('polls:detail', args=(past_question.id,))
+    )
+
+    assert response.status_code == 200
+    assert question_text.encode() in response.content
+
+
+@pytest.mark.django_db
+def test_question_details_not_returned_if_its_from_the_future(client):
+    past_question = create_question('whatever', days_old=-3)
+
+    response = client.get(
+        reverse('polls:detail', args=(past_question.id,))
+    )
+
+    assert response.status_code == 404
