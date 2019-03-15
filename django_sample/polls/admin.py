5959eaab07e7a7bcb636393722666e7e31ceaea2
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2019-03-15 22:05:55 +0000

    Django sample: part 7

--- django_sample/polls/admin.py
+++ django_sample/polls/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    inlines = [ChoiceInline]
+
+    list_display = ('text', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date']
+
+    search_fields = ['text']
+
+    date_hierarchy = 'pub_date'
+
+
+admin.site.register(Question, QuestionAdmin)
--- django_sample/polls/models.py
+++ django_sample/polls/models.py
@@ -14,6 +14,10 @@ class Question(models.Model):
             return True
         return False
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
