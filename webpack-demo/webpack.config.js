9bba2b7ddfb2d844219e3a2210c0fadab528c8c4
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2019-11-01 14:28:51 +0100

    Webpack: fonts are working.

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -14,11 +14,27 @@ module.exports = {
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: "url-loader?limit=10000&mimetype=application/font-woff",
+        use: [
+          {
+            loader: "url-loader?limit=10000&mimetype=application/font-woff",
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          },
+        ]
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: "file-loader",
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       },
     ]
   },
