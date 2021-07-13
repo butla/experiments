4228ec5abe998d4156d4520e23a42aa95e3aade4
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2021-07-13 20:09:21 +0200

    Toga GUI: expanding the test app

--- GUIs/beeware_toga_test_app/src/beeware_toga_test_app/app.py
+++ GUIs/beeware_toga_test_app/src/beeware_toga_test_app/app.py
@@ -5,6 +5,19 @@ import sys
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
+from toga.images import Image
+
+# from https://github.com/beeware/toga/blob/master/examples/table/table/app.py
+bee_movies = [
+    ('The Secret Life of Bees', '2008', '7.3', 'Drama'),
+    ('Bee Movie', '2007', '6.1', 'Animation, Adventure, Comedy'),
+    ('Bees', '1998', '6.3', 'Horror'),
+    ('The Girl Who Swallowed Bees', '2007', '7.5', 'Short'),
+    ('Birds Do It, Bees Do It', '1974', '7.3', 'Documentary'),
+    ('Bees: A Life for the Queen', '1998', '8.0', 'TV Movie'),
+    ('Bees in Paradise', '1944', '5.4', 'Comedy, Musical'),
+    ('Keeper of the Bees', '1947', '6.3', 'Drama')
+]
 
 
 class BeewareTogaTestApp(toga.App):
@@ -16,38 +29,53 @@ class BeewareTogaTestApp(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box(style=Pack(direction=COLUMN))
-
-        name_label = toga.Label(
-            'Your name: ',
-            style=Pack(padding=(0, 5))
-        )
-        self.name_input = toga.TextInput(style=Pack(flex=1))
-
-        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
-        name_box.add(name_label)
-        name_box.add(self.name_input)
-
-        button = toga.Button(
-            'Say Hello!',
-            on_press=self.say_hello,
-            style=Pack(padding=5)
-        )
-
-        main_box.add(name_box)
-        main_box.add(button)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        self.main_window.content = self._prepare_main_box()
         self.main_window.show()
-        import ptpython
-        print(ptpython.__file__)
 
-    def say_hello(self, widget):
-        self.main_window.info_dialog(
-            'Hi there!',
-            "Hello, {}".format(self.name_input.value)
+        # this is testing for using external Python dependencies
+        # import ptpython
+        # print(ptpython.__file__)
+
+    def _prepare_main_box(self):
+        container = toga.OptionContainer()
+
+        self.table = toga.Table(
+            headings=['Title', 'Year', 'Genre', 'Rating'],
+            data=bee_movies,
+            style=Pack(flex=1, padding_right=5),
+            multiple_select=False,
         )
+
+        open_image_button = toga.Button(
+            'Open Image',
+            on_press=self._open_image,
+            style=Pack(padding=5)
+        )
+        self.image_view = toga.ImageView(style=Pack(flex=1))
+        image_view_box = toga.Box(
+            children=[open_image_button, self.image_view],
+            style=Pack(direction=COLUMN),
+        )
+
+        container.add('Table', self.table)
+        container.add('Image page', image_view_box)
+
+        main_box = toga.Box(style=Pack(direction=COLUMN))
+        main_box.add(container)
+
+        return main_box
+
+    def _open_image(self, source_button):
+        # TODO no idea how to draw on an image...
+        # free-form GUI object placement would help, or Canvas with a background
+        try:
+            selected_file = self.main_window.open_file_dialog('Open image file')
+        except ValueError:
+            print('no image chosen')
+            return
+        self.image_view.image = Image(selected_file)
 
 
 def main():
