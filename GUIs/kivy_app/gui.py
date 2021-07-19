0b0b3887cb02370a801f0f6ea2e9f4f602d30dbc
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2021-07-19 20:25:18 +0200

    GUIs: more stuff

--- GUIs/kivy_app/gui.py
+++ GUIs/kivy_app/gui.py
@@ -2,14 +2,57 @@ import kivy
 kivy.require('2.0.0') # replace with your current kivy version !
 
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import NumericProperty, StringProperty
 from kivy.uix.label import Label
 
+KV = '''
+FloatLayout:
+    BoxLayout:
+        size_hint: .5, .5
+        pos_hint: {'center': (.5, .5)}
 
+        orientation: 'vertical'
+
+        TextInput:
+            text: app.text
+            on_text: app.text = self.text
+            id: input_box
+
+        Slider:
+            min: 0
+            max: 100
+            value: app.number
+            on_value: app.number = self.value
+
+        Button:
+            text: 'Jakiś guzik żółć A'
+            canvas:
+                Color:
+                    rgba: 1, .3, .8, .5
+                Line:
+                    points: [(15, 30), (300, 150)]
+
+        Button:
+            text: input_box.text
+'''
+
+# TODO make the app async
 class MyApp(App):
+    number = NumericProperty()
+    text = StringProperty()
 
     def build(self):
-        return Label(text='Hello world')
+        return Builder.load_string(KV)
 
 
+
+# TODO load the kv file and try changing some widget's canvas
+# MyWidget:
+#     canvas:
+#         Color:
+#             rgba: 1, .3, .8, .5
+#         Line:
+#             points: zip(self.data.x, self.data.y)
 if __name__ == '__main__':
     MyApp().run()
---
+++ GUIs/wxpython_app/gui.py
@@ -0,0 +1,29 @@
+# sample taken from https://wiki.wxpython.org/Getting%20Started
+
+import wx
+
+class MainWindow(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(600,300))
+        wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar() # A Statusbar in the bottom of the window
+
+        # Setting up the menu.
+        filemenu= wx.Menu()
+
+        # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
+        filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+
+        # Creating the menubar.
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+        self.Show(True)
+
+        # TODO add some buttons or a layout
+
+app = wx.App(False)
+frame = MainWindow(None, "Sample editor")
+app.MainLoop()
