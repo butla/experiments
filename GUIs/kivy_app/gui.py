4b708c3348790829c7d044f87b268ae1807c6232
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2021-07-20 21:31:50 +0200

    Kivy GUI: more stuff

--- GUIs/kivy_app/gui.py
+++ GUIs/kivy_app/gui.py
@@ -2,10 +2,12 @@ import kivy
 kivy.require('2.0.0') # replace with your current kivy version !
 
 from kivy.app import App
+import kivy.graphics
 from kivy.lang import Builder
 from kivy.properties import NumericProperty, StringProperty, ObjectProperty
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
 from kivy.uix.screenmanager import Screen, ScreenManager
 
 KV = '''
@@ -119,9 +121,14 @@ KV = '''
                     on_press: app.open_image()
 
 <ImageViewScreen>:
-    Button:
-        text: 'Go back'
-        on_press: root.manager.current = 'clients'
+    BoxLayout:
+        orientation: 'vertical'
+        Button:
+            text: 'Go back'
+            on_press: root.manager.current = 'clients'
+
+        ImageCanvas:
+            source: 'hippo.jpg'
 '''
 
 
@@ -134,6 +141,17 @@ class ClientListScreen(Screen):
 class ImageViewScreen(Screen):
     # TODO fill that table_box from sqlite. Might have to be made into a recycle view
     table_box = ObjectProperty()
+
+    # an example of how to use file chooser is in Kivy repo: kivy/examples/RST_Editor/main.py
+
+class ImageCanvas(Image):
+    def on_touch_down(self, touch):
+        with self.canvas:
+            kivy.graphics.Color(0, 0.7, 0)
+            dot_size = 20
+            dot_position = (touch.pos[0] - dot_size//2, touch.pos[1] - dot_size//2)
+            kivy.graphics.Ellipse(pos=dot_position, size=(dot_size, dot_size))
+        print('dupa', touch)
 
 
 # TODO make the app async
