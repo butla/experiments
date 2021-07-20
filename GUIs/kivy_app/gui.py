71a8bf3955a3f0047ee2076c791e9c9f59e9abcd
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2021-07-20 20:01:53 +0200

    Kivy GUI: switching between screens, slightly nicer table

--- GUIs/kivy_app/gui.py
+++ GUIs/kivy_app/gui.py
@@ -3,56 +3,154 @@ kivy.require('2.0.0') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import NumericProperty, StringProperty
+from kivy.properties import NumericProperty, StringProperty, ObjectProperty
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 KV = '''
-FloatLayout:
+<ClientListScreen>:
     BoxLayout:
-        size_hint: .5, .5
-        pos_hint: {'center': (.5, .5)}
-
         orientation: 'vertical'
+        table_box: table_box
 
-        TextInput:
-            text: app.text
-            on_text: app.text = self.text
-            id: input_box
+        BoxLayout:
+            orientation: 'horizontal'
 
-        Slider:
-            min: 0
-            max: 100
-            value: app.number
-            on_value: app.number = self.value
+            Label:
+                size_hint: 0.25, None
+                text: 'Nazwisko'
 
-        Button:
-            text: 'Jakiś guzik żółć A'
-            canvas:
-                Color:
-                    rgba: 1, .3, .8, .5
-                Line:
-                    points: [(15, 30), (300, 150)]
+            Label:
+                size_hint: 0.25, None
+                text: 'Imię'
 
-        Button:
-            text: input_box.text
+            Label:
+                size_hint: 0.25, None
+                text: 'wzrost'
+
+            Label:
+                size_hint: 0.25, None
+                text: 'Akcje'
+
+
+        BoxLayout:
+            orientation: 'vertical'
+            id: table_box
+
+            BoxLayout:
+                orientation: 'horizontal'
+
+                Label:
+                    size_hint: 0.25, None
+                    text: 'Mejer'
+
+                Label:
+                    size_hint: 0.25, None
+                    text: 'Zdzichu'
+
+                Label:
+                    size_hint: 0.25, None
+                    text: '173'
+
+                Button:
+                    size_hint: 0.25, None
+                    text: 'otwórz'
+                    on_press: app.open_image()
+
+            BoxLayout:
+                orientation: 'horizontal'
+
+                Label:
+                    size_hint: 0.25, None
+                    text: 'Ptok'
+
+                Label:
+                    size_hint: 0.25, None
+                    text: 'Kacper'
+
+                Label:
+                    size_hint: 0.25, None
+                    text: '180'
+
+                Button:
+                    size_hint: 0.25, None
+                    text: 'otwórz'
+                    on_press: app.open_image()
+
+            BoxLayout:
+                orientation: 'horizontal'
+
+                Label:
+                    size_hint: 0.25, None
+                    text: 'Ufnal'
+
+                Label:
+                    size_hint: 0.25, None
+                    text: 'Melania'
+
+                Label:
+                    size_hint: 0.25, None
+                    text: '169'
+
+                Button:
+                    size_hint: 0.25, None
+                    text: 'otwórz'
+                    on_press: app.open_image()
+
+            BoxLayout:
+                orientation: 'horizontal'
+
+                Label:
+                    size_hint: 0.25, None
+                    text: 'Cyroń'
+
+                Label:
+                    size_hint: 0.25, None
+                    text: 'Marcin'
+
+                Label:
+                    size_hint: 0.25, None
+                    text: '183'
+
+                Button:
+                    size_hint: 0.25, None
+                    text: 'otwórz'
+                    on_press: app.open_image()
+
+<ImageViewScreen>:
+    Button:
+        text: 'Go back'
+        on_press: root.manager.current = 'clients'
 '''
+
+
+# TODO can I reference objects in the layout directly from the app object?
+class ClientListScreen(Screen):
+    # TODO fill that table_box from sqlite. Might have to be made into a recycle view
+    table_box = ObjectProperty()
+
+
+class ImageViewScreen(Screen):
+    # TODO fill that table_box from sqlite. Might have to be made into a recycle view
+    table_box = ObjectProperty()
+
 
 # TODO make the app async
 class MyApp(App):
-    number = NumericProperty()
-    text = StringProperty()
 
     def build(self):
-        return Builder.load_string(KV)
+        Builder.load_string(KV)
+
+        self.screen_manager = ScreenManager()
+        self.screen_manager.add_widget(ClientListScreen(name='clients'))
+        self.screen_manager.add_widget(ImageViewScreen(name='image_view'))
+
+        return self.screen_manager
+
+    def open_image(self):
+        self.screen_manager.current = 'image_view'
 
 
-
-# TODO load the kv file and try changing some widget's canvas
-# MyWidget:
-#     canvas:
-#         Color:
-#             rgba: 1, .3, .8, .5
-#         Line:
-#             points: zip(self.data.x, self.data.y)
 if __name__ == '__main__':
     MyApp().run()
