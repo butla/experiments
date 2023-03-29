2c4dca8b9ff5b3ebbbe4fcfb46d81bf894221067
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2023-03-29 17:42:04 +0200

    Sample backend app: configurable local container ports.

--- testing__quality_assurance/sample_backend_app/tests/external/test_app_features.py
+++ testing__quality_assurance/sample_backend_app/tests/external/test_app_features.py
@@ -2,21 +2,22 @@
 This is a sample external tests' file.
 """
 
+import os
 import uuid
 
 import httpx
 import pytest
 import tenacity
 
-APP_ADDRESS = "http://localhost:8080"
-
 
 # Scope is session, so that this fixture runs only once during the test suite.
 # If we'd have more tests, we don't need to go through waiting for the app again.
 @pytest.fixture(scope="session")
 def app_url():
-    _wait_for_http_url(APP_ADDRESS)
-    return APP_ADDRESS
+    app_port = os.environ.get("API_PORT", 8080)
+    app_address = f"http://localhost:{app_port}"
+    _wait_for_http_url(app_address)
+    return app_address
 
 
 @tenacity.retry(stop=tenacity.stop_after_delay(10), wait=tenacity.wait_fixed(0.2), reraise=True)
@@ -29,16 +30,16 @@ def _wait_for_http_url(url: str):
 def test_store_and_retrieve_note(app_url):
     note_contents = f"first note {uuid.uuid4()}"
     create_result = httpx.post(
-        f"{APP_ADDRESS}/notes/",
+        f"{app_url}/notes/",
         json={"contents": note_contents},
     )
     assert create_result.status_code == 201
     assert create_result.json()["contents"] == note_contents
     note_id = create_result.json()["id"]
 
-    get_result = httpx.get(f"{APP_ADDRESS}/notes/{note_id}/")
+    get_result = httpx.get(f"{app_url}/notes/{note_id}/")
     assert get_result.status_code == 200
     assert get_result.json()["contents"] == note_contents
 
-    get_all_result = httpx.get(f"{APP_ADDRESS}/notes/")
+    get_all_result = httpx.get(f"{app_url}/notes/")
     assert next(note for note in get_all_result.json() if note["id"] == note_id)
